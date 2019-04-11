9af1b02cc0d9889586013697249ebee7da6c3eec
jwlodek <jwlodek@cs.stonybrook.edu>
2019-04-11 13:29:37 -0400

    working on menus

--- public/benchmark2/js/load.js
+++ public/benchmark2/js/load.js
@@ -9,6 +9,7 @@ ThroughTheAges.Load.prototype = {
 
         //load all sprites and images and spritesheets here
         this.load.image('MainMenu', 'Main Menu.png');
+        this.load.image('Help', 'User_Help_Menu.png');
     },
     update: function(){
         if(this.game.input.activePointer.justPressed()){
--- public/benchmark2/js/menus.js
+++ public/benchmark2/js/menus.js
@@ -8,7 +8,52 @@ ThroughTheAges.LevelSelect = function(){};
 ThroughTheAges.MainMenu.prototype = {
     create: function(){
         console.log("Entered main menu create function");
-        this.background = this.add.image(0, 0, this.game.width, this.game.height, 'MainMenu');
+        this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'MainMenu');
+        this.splash.anchor.setTo(0.5);
         console.log("Background loaded");
     },
+    update: function(){
+        if(this.game.input.activePointer.justPressed()){
+            console.log(this.game.input.activePointer);
+        }
+    }
+};
+
+ThroughTheAges.Help.prototype = {
+    create: function(){
+        console.log("Entered help create function");
+        this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'Help');
+        this.splash.anchor.setTo(0.5);
+    },
+    update: function(){
+        if(this.game.input.activePointer.justPressed()){
+            this.game.state.start('MainMenu');
+        }
+    }
+};
+
+ThroughTheAges.LevelSelect.prototype = {
+    create: function(){
+        console.log("Entered help create function");
+        this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'LevelSelect');
+        this.splash.anchor.setTo(0.5);
+    },
+    update: function(){
+        if(this.game.input.activePointer.justPressed()){
+            this.game.state.start('Game');
+        }
+    }
+};
+
+ThroughTheAges.Pause.prototype = {
+    create: function(){
+        console.log("Entered pause create function");
+        this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'Help');
+        this.splash.anchor.setTo(0.5);
+    },
+    update: function(){
+        if(this.game.input.activePointer.justPressed()){
+            this.game.state.start('Game');
+        }
+    }
 };
